3acfd1831123a2766b00a68c146f4cdf33c30065
Katrinka <kjswenso@gmail.com>
2022-02-01 17:13:37 -0500

    added all sample audio and mapped to svg as well as tooltips to display title

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 // const controllers = document.querySelectorAll('polygon');
 const controllers = document.querySelectorAll('.sample-sound');
 controllers.forEach(sample => sample.addEventListener('click', changeAudio));
+controllers.forEach(sample => sample.addEventListener('mouseenter', showTooltip));
+controllers.forEach(sample => sample.addEventListener('mouseleave', hideTooltip));
+
 
 function changeAudio(e) {
     const elem = e.srcElement;
@@ -13,4 +16,18 @@ function changeAudio(e) {
         audioSample.currentTime = 0;
         elem.dataset.isplaying = false;
      }
+}
+
+function showTooltip(e) {
+    const elem = e.srcElement;
+    const toolTip = document.getElementById('tooltip');
+    toolTip.innerHTML = `Click to pause/play a sample of ${elem.dataset.title}`;
+    toolTip.style.display = 'block';
+    toolTip.style.left = e.pageX + 10 + 'px';
+    toolTip.style.top = e.pageY + 10 + 'px';
+}
+
+function hideTooltip() {
+    const toolTip = document.getElementById('tooltip');
+    toolTip.style.display = 'none';
 }
